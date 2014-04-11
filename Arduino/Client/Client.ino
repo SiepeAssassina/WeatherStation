39ece0073e13531c43f45975c923a31c0f99fe39
SiepeAssassina <lorenzo.tomasin@live.it>
2014-04-11 16:09:07 +0200

    Alpha 0.0.1

--- Arduino/Client/Client.ino
+++ Arduino/Client/Client.ino
@@ -1,8 +1,14 @@
 //Client
 void setup()
 {
+  Serial.begin(1200, SERIAL_8E1);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, 0);
 }
 
 void loop()
 {
+  int data = 1023;
+  data = data >> 8;
+  Serial.write(data);
 }
--- Arduino/Server/Server.ino
+++ Arduino/Server/Server.ino
@@ -1,8 +1,55 @@
 //Server
+#define IDLE 0
+#define ACK 1
+#define DATA_RX 2
+#define DATA_CHUNKS 2
+#define SENSORS 4
+char dataChunks[DATA_CHUNKS];
+int data[4];
+int serialState = IDLE;
 void setup()
 {
+  Serial.begin(1200);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, 0);
 }
 
 void loop()
-{
+{   
+  waitForSerial();  
+  switch(Serial.read())
+  {
+  case 'D':
+    {
+      serialState = DATA_RX;
+      break;
+    }        
+  }
+  while(serialState == DATA_RX)
+  {
+    receiveData();
+  }
 }
+
+void waitForSerial()
+{
+  while(Serial.available() == 2)
+  {
+    if(Serial.read() == 'A' && Serial.read() == 'A');
+      serialState = ACK;
+  }
+}
+
+void receiveData()
+{
+  Serial.write('C');
+  char _chunks = Serial.read();
+  while(!Serial.available());            
+  dataChunks[i] = Serial.read();
+  Serial.write('N');    
+}
+
+
+
+
+
