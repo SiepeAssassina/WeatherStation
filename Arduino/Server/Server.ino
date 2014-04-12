ecb52f2e8b91a275a557fd58fb7080c0e91df8ec
SiepeAssassina <lorenzo.tomasin@live.it>
2014-04-12 10:45:30 +0200

    Alpha 0.0.2
    Added PC comunication

--- Arduino/Server/Server.ino
+++ Arduino/Server/Server.ino
@@ -1,53 +1,84 @@
 //Server
-#define IDLE 0
-#define ACK 1
-#define DATA_RX 2
-#define DATA_CHUNKS 2
+#include <Time.h>
+#define WAITING 0
+#define DATA_RX 1  //Client -> Server 
+#define DATA_TX 2  //Server -> PC
 #define SENSORS 4
-char dataChunks[DATA_CHUNKS];
-int data[4];
-int serialState = IDLE;
+byte hh = 0, mm = 0, ss = 0;
+sensorData data[4] = {
+  666, 231, 1022, 454};
+struct sensorData
+{
+  char sensorAdress;    //0-5 1byte
+  unsigned short value; //0-1023 2byte
+  
+};
+char serialState = WAITING;
 void setup()
 {
-  Serial.begin(1200);
+  Serial.begin(1200, SERIAL_8E1);
   pinMode(13, OUTPUT);
   digitalWrite(13, 0);
 }
 
 void loop()
-{   
-  waitForSerial();  
-  switch(Serial.read())
+{  
+  waitForPreamble(); 
+  while(serialState != WAITING)
   {
-  case 'D':
+    switch(serialState)
     {
-      serialState = DATA_RX;
-      break;
-    }        
-  }
-  while(serialState == DATA_RX)
-  {
-    receiveData();
+    case DATA_RX:
+      {
+        break;
+      }
+    case DATA_TX:      
+      {
+        sendSensorData();
+        serialState = WAITING;
+        digitalWrite(13, 0);
+        break;
+      }
+    } 
   }
 }
 
 void waitForSerial()
 {
-  while(Serial.available() == 2)
+  while(!Serial.available());
+}
+
+void waitForPreamble()
+{
+  while(Serial.available() == 3)
   {
-    if(Serial.read() == 'A' && Serial.read() == 'A');
-      serialState = ACK;
+    if(Serial.read() == 'A' && Serial.read() == 'A' && Serial.read() == 'P')
+    {
+      serialState = DATA_TX;
+      digitalWrite(13, 1);
+    }     
   }
 }
 
 void receiveData()
 {
-  Serial.write('C');
-  char _chunks = Serial.read();
-  while(!Serial.available());            
-  dataChunks[i] = Serial.read();
-  Serial.write('N');    
+
 }
+
+void sendSensorData()
+{
+  Serial.write(SENSORS);
+  delay(10);
+  for(int i = 0; i < SENSORS; i++)
+  {   
+    Serial.write((data[i] & 0x300) >> 8);
+    Serial.write(data[i] & 0xFF);
+  }
+}
+
+
+
+
 
 
 
