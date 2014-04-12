0af76dcf6a0b9bcf50c73308babd4792bebc9ddc
SiepeAssassina <lorenzo.tomasin@live.it>
2014-04-12 11:34:27 +0200

    Alpha 0.0.3
    Pc-server data transfer is nearly complete, still missing timestoring
    for values.
    Leaving PC while writing the client-side.
    Added C# debug code.

--- Arduino/Server/Server.ino
+++ Arduino/Server/Server.ino
@@ -3,26 +3,37 @@
 #define WAITING 0
 #define DATA_RX 1  //Client -> Server 
 #define DATA_TX 2  //Server -> PC
+#define TIME_SET 3
 #define SENSORS 4
 byte hh = 0, mm = 0, ss = 0;
-sensorData data[4] = {
-  666, 231, 1022, 454};
 struct sensorData
 {
-  char sensorAdress;    //0-5 1byte
-  unsigned short value; //0-1023 2byte
-  
+  char adress;    //0-5 1byte
+  unsigned short value; //0-1023 2byte  
 };
+sensorData data[4] = {
+  {
+    0, 1023        }
+  , {
+    1, 511        }
+  , {
+    2, 255        }
+  , {
+    3, 127        }
+};
+
 char serialState = WAITING;
 void setup()
 {
   Serial.begin(1200, SERIAL_8E1);
   pinMode(13, OUTPUT);
   digitalWrite(13, 0);
+  setTime(hh, mm, ss, 00, 00, 00);
 }
 
 void loop()
 {  
+  //byte timeFigures[4] = {hour() / 10, hour() % 10, minute() / 10, minute() % 10};
   waitForPreamble(); 
   while(serialState != WAITING)
   {
@@ -34,8 +45,16 @@ void loop()
       }
     case DATA_TX:      
       {
+         digitalWrite(13, 1);
         sendSensorData();
         serialState = WAITING;
+        digitalWrite(13, 0);
+        break;
+      }
+    case TIME_SET:
+      {
+        digitalWrite(13, 1);
+        getSerialTime();
         digitalWrite(13, 0);
         break;
       }
@@ -52,9 +71,21 @@ void waitForPreamble()
 {
   while(Serial.available() == 3)
   {
-    if(Serial.read() == 'A' && Serial.read() == 'A' && Serial.read() == 'P')
+    if(Serial.read() == 'A' && Serial.read() == 'A')
     {
-      serialState = DATA_TX;
+      switch(Serial.read())
+      {
+      case 'P':
+        {
+          serialState = DATA_TX;
+          break;
+        }
+      case 'T':
+        {
+          serialState = TIME_SET;
+          break;
+        }
+      }      
       digitalWrite(13, 1);
     }     
   }
@@ -71,10 +102,27 @@ void sendSensorData()
   delay(10);
   for(int i = 0; i < SENSORS; i++)
   {   
-    Serial.write((data[i] & 0x300) >> 8);
-    Serial.write(data[i] & 0xFF);
+    Serial.write(data[i].adress);
+    delay(100);
+    Serial.write((data[i].value & 0x300) >> 8);
+    delay(100);
+    Serial.write(data[i].value & 0xFF);
+    delay(100);
   }
 }
+
+void getSerialTime()
+{
+  while(!Serial.available() == 3);
+  hh = Serial.read();
+  mm = Serial.read();
+  ss = Serial.read();
+  setTime(hh, mm, ss, 00, 00, 00);
+  serialState = WAITING;
+}
+
+
+
 
 
 
---
+++ PC/SerialConsole/SerialConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+
+namespace SerialConsole
+{
+    class Program
+    {
+        static public SerialPort com;
+        static void Main(string[] args)
+        {
+            char[] buffer = new char[1];
+            com = new SerialPort("COM3");
+            com.BaudRate = 1200;
+            com.Parity = Parity.Even;
+            com.StopBits = StopBits.One;
+            com.Handshake = Handshake.None;
+            // com.ReadTimeout = 1000;
+            com.ReadBufferSize = 8;
+            com.Open();
+
+            while (true)
+            {
+                Console.Write("WAITING > ");
+                switch (Console.ReadLine())
+                {
+                    case "T":
+                        {
+                            sendTime();
+                            break;
+                        }
+                    case "S":
+                        {
+                            getSensorData();
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+
+        static void sendTime()
+        {
+            com.Write("A");
+            com.Write("A");
+            com.Write("T");
+            Console.WriteLine("Sending time " + string.Format("{0:HH:mm:ss tt}", DateTime.Now));
+            com.Write(DateTime.Now.Hour.ToString());
+            System.Threading.Thread.Sleep(100);
+            com.Write(DateTime.Now.Minute.ToString());
+            System.Threading.Thread.Sleep(100);
+            com.Write(DateTime.Now.Second.ToString());
+            System.Threading.Thread.Sleep(100);
+        }
+
+        static void waitForCom(char c)
+        {
+            while (com.ReadByte() != c) ;
+        }
+
+        static void getSensorData()
+        {
+            com.Write("A");
+            com.Write("A");
+            com.Write("P");
+            while (true)
+            {
+                byte c = (byte)com.ReadByte();
+                for (byte i = 0; i < c; i++)
+                {
+                    Console.Write((byte)com.ReadByte() + ": ");
+                    Console.WriteLine(((com.ReadByte() & 0x3) << 8) + com.ReadByte());
+                }
+            }
+        }
+    }
+}
